0c996d93c53e89e0df77bce84a7a12f7602b3ea8
Vadim Smirnov <vadim@ntkernel.com>
2023-07-08 12:20:33 +0200

    Update HTTP File Server Code to run two server instances returning files in "whole" and "chunked" modes. Updated File Server Test Scripts. 'fs-test-single.py' that tests the server operations using a single HTTP session. This script first tries to GET the file, and if it doesn't exist, the script uploads the file, then downloads it, and finally deletes it from the server. 'fs-test-multi.py', which performs the same operations as the 'fs-test-single.py' script, but instead, it opens a new HTTP session for each operation to mimic different client scenarios.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use actix_multipart::Multipart;
+use std::path::PathBuf;
 use actix_web::{delete, get, post, web, App, HttpResponse, HttpServer, Responder};
 use futures::{StreamExt, TryStreamExt};
 use sanitize_filename::sanitize;
@@ -7,9 +8,30 @@ use tokio::fs::File;
 use tokio::io::{self, AsyncBufReadExt, AsyncWriteExt, BufReader};
 use std::fs;
 use std::path::Path;
+use tokio_util::io::ReaderStream;
 
-/// Handles file uploads to the server. Uses the Multipart request payload to handle the uploaded file.
-/// The function sanitizes the filename to ensure security, then writes the received data to a new file.
+/// Handles file uploads to the server.
+///
+/// This function uses the `Multipart` request payload to process the uploaded file. It goes through each part
+/// of the payload until there are no more parts left.
+///
+/// If a part is a file (determined by the presence of a filename in the part's content-disposition),
+/// the function sanitizes the filename to prevent directory traversal attacks and other security issues.
+/// It then checks if a file with the same name already exists on the server.
+///
+/// If the file does not exist, the function creates a new file and writes the uploaded data to it.
+/// If the file does exist, the function returns a `Conflict` response and does not overwrite the existing file.
+///
+/// # Arguments
+///
+/// * `payload` - A mutable reference to a `Multipart` payload, which represents the uploaded file data.
+///
+/// # Returns
+///
+/// An `HttpResponse` which can be:
+/// * `Ok` with a success message as the body if the file was successfully uploaded.
+/// * `BadRequest` if the filename is invalid or empty.
+/// * `Conflict` if a file with the same name already exists on the server.
 #[post("/upload")]
 async fn upload(mut payload: Multipart) -> impl Responder {
     while let Ok(Some(mut field)) = payload.try_next().await {
@@ -31,8 +53,18 @@ async fn upload(mut payload: Multipart) -> impl Responder {
     HttpResponse::Ok().body("File uploaded successfully")
 }
 
-/// Handles download requests for files on the server.
-/// The function checks if the requested file exists, and if it does, returns the file's content.
+/// Handles download requests for files on the server by checking if the 
+/// requested file exists, and if it does, returns the file's content in its entirety.
+/// This may not be efficient for large files as it reads the entire file into memory.
+///
+/// # Arguments
+///
+/// * `filename` - A `web::Path<String>` representing the filename to download.
+///
+/// # Returns
+///
+/// An `HttpResponse` which can be `Ok` with the file's content as the body 
+/// or `NotFound` if the file doesn't exist.
 #[get("/{filename}")]
 async fn download(filename: web::Path<String>) -> impl Responder {
     let filename = sanitize(filename.into_inner());
@@ -46,8 +78,50 @@ async fn download(filename: web::Path<String>) -> impl Responder {
     }
 }
 
+/// Handles download requests for files on the server by checking if the 
+/// requested file exists, and if it does, returns the file's content in chunks.
+/// This is efficient for large files as it streams the file in chunks rather than reading the 
+/// entire file into memory.
+///
+/// # Arguments
+///
+/// * `path` - A `web::Path<String>` representing the path to the file to download.
+///
+/// # Returns
+///
+/// An `HttpResponse` which can be `Ok` with a `Stream` of the file's content as the body,
+/// `InternalServerError` if there was a problem reading the file,
+/// or `NotFound` if the file doesn't exist.
+#[get("/{filename:.*}")]
+async fn chunked_download(path: web::Path<String>) -> impl Responder {
+    let filename = sanitize(path.into_inner());
+    let file_path = PathBuf::from("./").join(filename);
+
+    if file_path.exists() {
+        match File::open(&file_path).await {
+            Ok(file) => HttpResponse::Ok().streaming(ReaderStream::new(file)),
+            Err(_) => HttpResponse::InternalServerError().body("Could not read file"),
+        }
+    } else {
+        HttpResponse::NotFound().body("File not found")
+    }
+}
+
 /// Handles delete requests for files on the server.
-/// The function checks if the requested file exists, and if it does, deletes the file.
+///
+/// This function sanitizes the provided filename and checks if the file exists on the server.
+/// If the file exists, it is deleted from the server. If the file does not exist, a response
+/// indicating the file was not found is returned.
+///
+/// # Arguments
+///
+/// * `filename` - A `web::Path<String>` representing the filename to delete.
+///
+/// # Returns
+///
+/// An `HttpResponse` which can be:
+/// * `Ok` with a success message as the body if the file was successfully deleted.
+/// * `NotFound` if the file does not exist on the server.
 #[delete("/{filename}")]
 async fn delete(filename: web::Path<String>) -> impl Responder {
     let filename = sanitize(filename.into_inner());
@@ -61,17 +135,33 @@ async fn delete(filename: web::Path<String>) -> impl Responder {
     }
 }
 
-/// Main function to set up the server and start listening for requests.
-/// The server listens on a user-specified port, or defaults to 3000 if no port is specified.
+/// Main function to set up two servers and start listening for requests on sequential ports.
+/// The first server uses a flat download method, while the second uses a chunked download method.
+///
+/// Servers listen on a user-specified base port and the subsequent port number (base port + 1),
+/// or default to 3000 and 3001 if no base port is specified.
+///
+/// Pressing ENTER will cause both servers to shut down.
+///
+/// # Returns
+///
+/// An `std::io::Result<()>` which is the result of the server operations. If successful,
+/// the function will return `Ok(())`.
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let port = env::args().nth(1).unwrap_or_else(|| "3000".to_string());
-    let bind_address = format!("0.0.0.0:{}", port);
-    println!("Listening on http://{}", bind_address);
-    println!("Press ENTER to exit");
+    // Get base port from command-line argument, or default to 3000.
+    let base_port = env::args().nth(1).map(|s| s.parse::<u16>().unwrap()).unwrap_or(3000);
 
+    // Set up bind addresses for both servers.
+    let bind_address1 = format!("0.0.0.0:{}", base_port);
+    println!("Listening on http://{}", bind_address1);
+    let bind_address2 = format!("0.0.0.0:{}", base_port + 1);
+    println!("Listening on http://{}", bind_address2);
+
+    // Set up a one-shot channel for exiting the servers.
     let (tx, rx) = tokio::sync::oneshot::channel();
 
+    // Start a task that waits for the ENTER key to be pressed.
     tokio::spawn(async move {
         let mut reader = BufReader::new(io::stdin());
         let mut buffer = String::new();
@@ -79,17 +169,30 @@ async fn main() -> std::io::Result<()> {
         tx.send(()).unwrap();
     });
 
-    let server = HttpServer::new(|| {
+    // Start the first server with the flat download handler.
+    let server1 = HttpServer::new(|| {
         App::new()
             .service(upload)
-            .service(download)
+            .service(download) // Flat download
             .service(delete)
     })
-    .bind(&bind_address)?
+    .bind(&bind_address1)?
     .run();
 
+    // Start the second server with the chunked download handler.
+    let server2 = HttpServer::new(|| {
+        App::new()
+            .service(upload)
+            .service(chunked_download) // Chunked download
+            .service(delete)
+    })
+    .bind(&bind_address2)?
+    .run();
+
+    // Wait for either server to finish, or for the ENTER key to be pressed.
     tokio::select! {
-        _ = server => {},
+        _ = server1 => {},
+        _ = server2 => {},
         _ = rx => {
             println!("ENTER pressed, shutting down");
         }
---
+++ test/fs-test-multi.py
@@ -0,0 +1,81 @@
+import sys
+import requests
+import random
+import string
+import time
+
+def generate_random_text_file(filename, size):
+    block_size = 1024
+    block = ''.join(random.choices(string.ascii_letters + string.digits, k=block_size))
+
+    with open(filename, 'w') as file:
+        generated_size = 0
+        while generated_size < size:
+            chunk_size = min(size - generated_size, block_size)
+            file.write(block[:chunk_size])
+            generated_size += chunk_size
+
+def upload_file(filename, server_url):
+    url = f'{server_url}/upload'
+    with open(filename, 'rb') as f:
+        files = {'file': f}
+        response = requests.post(url, files=files)
+    return response.status_code
+
+def download_file(filename, server_url):
+    url = f'{server_url}/{filename}'
+    response = requests.get(url)
+    if response.status_code == 200:
+        with open(filename, 'wb') as file:
+            file.write(response.content)
+    return response.status_code
+
+def delete_file(filename, server_url):
+    url = f'{server_url}/{filename}'
+    response = requests.delete(url)
+    return response.status_code
+
+def main():
+    if len(sys.argv) < 4:
+        print('Please provide a filename, file size and server URL as command line arguments')
+        return
+
+    filename = sys.argv[1]
+    size = int(sys.argv[2])
+    server_url = sys.argv[3]
+
+    # Try to download the file
+    start_time = time.time()
+    download_status = download_file(filename, server_url)
+    end_time = time.time()
+    if download_status == 200:
+        print(f'File downloaded: {filename}, Status: {download_status}, Time taken: {end_time - start_time:.2f}s')
+    else:
+        print('File does not exist on the server. Uploading the file...')
+        # Generate a random text file
+        start_time = time.time()
+        generate_random_text_file(filename, size)
+        end_time = time.time()
+        print(f'File generated: {filename}, Size: {size} bytes, Time taken: {end_time - start_time:.2f}s')
+
+        # Upload the file
+        start_time = time.time()
+        upload_status = upload_file(filename, server_url)
+        end_time = time.time()
+        print(f'File uploaded: {filename}, Status: {upload_status}, Time taken: {end_time - start_time:.2f}s')
+        
+        # Download the file
+        start_time = time.time()
+        download_status = download_file(filename, server_url)
+        end_time = time.time()
+        print(f'File downloaded: {filename}, Status: {download_status}, Time taken: {end_time - start_time:.2f}s')
+
+    # Delete the file
+    start_time = time.time()
+    delete_status = delete_file(filename, server_url)
+    end_time = time.time()
+    print(f'File deleted: {filename}, Status: {delete_status}, Time taken: {end_time - start_time:.2f}s')
+
+if __name__ == '__main__':
+    main()
+
